3dd327f2278dad738a3fdfe1f4c42d377dc8308f
GrishaOvs <grisha.ovsyannikov.03@mail.ru>
2019-07-03 16:53:13 +0300

    tas9660 done

--- ege25/ege25/task9660.cpp
+++ ege25/ege25/task9660.cpp
@@ -2,9 +2,26 @@
 
 void task9660()
 {
-	int i = 0;
-	for (i = 0; i < 5; i++)
+	int const n = 5;
+	int a[n];
+	for (int i = 0; i < n; i++)
 	{
-		cout << 5;
+		cin >> a[i];
+	}
+	int maxDoubleDigit = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < 100 && a[i] > 9 && a[i] % 3 != 0 && a[i] >  maxDoubleDigit)
+		{
+			maxDoubleDigit = a[i];
+		}
+	}
+	if (maxDoubleDigit == 0)
+	{
+		cout << "not found";
+	}
+	else
+	{
+		cout << maxDoubleDigit;
 	}
 }
